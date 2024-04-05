ba5a34e82a9e7bc044357d7ece34d977390e10b5
RozKoy <okkoy.1401@gmail.com>
2024-04-05 13:04:41 +0700

    feat(view): login page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/masuk', function () {
+    return view('authentication.login');
+})->name('login');
