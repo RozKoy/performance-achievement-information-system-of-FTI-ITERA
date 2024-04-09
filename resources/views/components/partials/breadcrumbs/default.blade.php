91d910fb9288d7f14db3dffd965545ee3bb42753
RozKoy <okkoy.1401@gmail.com>
2024-04-09 22:19:27 +0700

    feat(breadcrumbs): add title

--- resources/views/components/partials/breadcrumbs/default.blade.php
+++ resources/views/components/partials/breadcrumbs/default.blade.php
@@ -1,6 +1,6 @@
 <nav class="flex" aria-label="Breadcrumb">
     <ol class="inline-flex items-center space-x-1 rtl:space-x-reverse md:space-x-2">
-        <li class="inline-flex items-center">
+        <li title="Halaman beranda" class="inline-flex items-center">
             <a href="{{ url(route('super-admin-dashboard')) }}" class="@if (request()->routeIs('super-admin-dashboard')) pointer-events-none text-primary/75 @else text-gray-500 hover:text-primary @endif inline-flex items-center text-sm font-medium">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="me-2.5 h-3 w-3">
                     <path d="M22.849,7.68l-.869-.68h.021V2h-2v3.451L13.849,.637c-1.088-.852-2.609-.852-3.697,0L1.151,7.68c-.731,.572-1.151,1.434-1.151,2.363v13.957H9V15c0-.551,.448-1,1-1h4c.552,0,1,.449,1,1v9h9V10.043c0-.929-.42-1.791-1.151-2.363Zm-.849,14.32h-5v-7c0-1.654-1.346-3-3-3h-4c-1.654,0-3,1.346-3,3v7H2V10.043c0-.31,.14-.597,.384-.788L11.384,2.212c.363-.284,.869-.284,1.232,0l9,7.043c.244,.191,.384,.478,.384,.788v11.957Z" />
@@ -10,7 +10,7 @@
         </li>
         @isset($breadCrumbs)
             @foreach ($breadCrumbs as $item)
-                <li>
+                <li title="Halaman {{ $item['name'] }}">
                     <div class="flex items-center">
                         <svg class="mx-1 h-3 w-3 text-gray-400 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
