2777bc5d01a07cc3fc48d6e78ccf49786e9922e2
RozKoy <okkoy.1401@gmail.com>
2024-04-09 19:37:56 +0700

    feat(component): breadcrumbs

--- resources/views/super-admin/organization/home.blade.php
+++ resources/views/super-admin/organization/home.blade.php
@@ -1,8 +1,14 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+@php
+    $breadCrumbs = [
+        [
+            'link' => 'super-admin-organization',
+            'name' => 'Manajemen organisasi',
+        ],
+    ];
+@endphp
 <x-super-admin-template title="Organisasi - Super Admin">
     <div class="flex flex-col gap-5 2xl:mx-auto 2xl:max-w-[2000px]">
+        <x-partials.breadcrumbs.default :$breadCrumbs />
         <h2 title="Halaman manajemen organisasi" class="text-xl font-semibold text-primary sm:text-2xl">Manajemen Organisasi</h2>
         <div class="flex gap-3 max-sm:flex-col">
             <form class="m-0 flex w-full">
