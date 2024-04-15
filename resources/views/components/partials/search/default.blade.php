9b53a38fd58ba6305498d28b055fc5bd7d9f8ce0
RozKoy <okkoy.1401@gmail.com>
2024-04-15 14:02:07 +0700

    feat(search action): auto submit

--- resources/views/components/partials/search/default.blade.php
+++ resources/views/components/partials/search/default.blade.php
@@ -1,4 +1,9 @@
 <form class="m-0 flex w-full">
+    @foreach (request()->query() as $index => $item)
+        @if ($index !== 'search')
+            <input type="hidden" name="{{ $index }}" value="{{ $item }}">
+        @endif
+    @endforeach
     <div class="relative flex-1 text-primary">
         <div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="aspect-square w-3 sm:w-4" aria-hidden="true">
@@ -7,7 +12,21 @@
                 </g>
             </svg>
         </div>
-        <input type="search" name="search" title="Pencarian" oninvalid="this.setCustomValidity('Pencarian wajib diisi')" oninput="this.setCustomValidity('')" class="block w-full flex-1 rounded-l-lg border-2 !border-slate-100 px-2 py-1.5 !ps-8 text-xs text-primary focus:!border-primary focus:ring-0 sm:px-2.5 sm:ps-10 sm:text-sm" placeholder="Cari..." required />
+        <input type="search" name="search" title="Pencarian" oninvalid="this.setCustomValidity('Pencarian wajib diisi')" oninput={{ request()->query('search') !== null ? 'clearSearch(this)' : "this.setCustomValidity('')" }} class="block w-full flex-1 rounded-l-lg border-2 !border-slate-100 px-2 py-1.5 !ps-8 text-xs text-primary focus:!border-primary focus:ring-0 sm:px-2.5 sm:ps-10 sm:text-sm" value="{{ request()->query('search') }}" placeholder="Cari..." />
     </div>
     <button type="submit" title="Tombol cari" class="rounded-r-lg !bg-primary/80 px-2 text-xs font-medium text-white hover:!bg-primary/70 focus:outline-none focus:ring-2 focus:ring-primary sm:px-4 sm:text-sm">Cari</button>
 </form>
+
+@pushIf(request()->query('search') !== null, 'script')
+@pushOnce('script')
+    <script>
+        function clearSearch(element) {
+            if (element.value === '') {
+                element.form.submit();
+            } else {
+                element.setCustomValidity('');
+            }
+        }
+    </script>
+@endPushOnce
+@endPushIf
