6a3e27f960cf486b25c9b69784dc415adb8184c0
RozKoy <okkoy.1401@gmail.com>
2024-04-16 21:27:45 +0700

    fix(component): style of edit button

--- resources/views/components/partials/button/edit.blade.php
+++ resources/views/components/partials/button/edit.blade.php
@@ -1,5 +1,5 @@
 @if (isset($link))
-    <a href="{{ url($link) }}" title="Ubah" class="rounded-full bg-yellow-500 p-0.5 text-white hover:bg-yellow-400">
+    <a href="{{ url($link) }}" title="Ubah" class="{{ isset($style) ? $style : '' }} rounded-full bg-yellow-500 p-0.5 text-white hover:bg-yellow-400">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="aspect-square w-5 sm:w-6">
             <path d="m12,0C5.383,0,0,5.383,0,12s5.383,12,12,12,12-5.383,12-12S18.617,0,12,0Zm0,22c-5.514,0-10-4.486-10-10S6.486,2,12,2s10,4.486,10,10-4.486,10-10,10Zm1.379-15.621l-7.379,7.379v4.242h4.242l7.379-7.379c1.17-1.17,1.17-3.072,0-4.242s-3.072-1.17-4.242,0Zm-3.965,9.621h-1.414v-1.414l4.336-4.336,1.414,1.414-4.336,4.336Zm6.793-6.793l-1.043,1.043-1.414-1.414,1.043-1.043c.391-.391,1.023-.391,1.414,0s.39,1.024,0,1.414Z" />
         </svg>
