6a9d4de8b87c6cc384af0305e003b13721329fbd
RozKoy <okkoy.1401@gmail.com>
2024-04-17 07:03:35 +0700

    fix(size): max width super admin template

--- resources/views/components/super-admin-template.blade.php
+++ resources/views/components/super-admin-template.blade.php
@@ -158,7 +158,7 @@
 
     <div class="p-4 sm:ml-72">
         <div class="mt-14 rounded-lg border-2 border-dashed border-primary p-4">
-            <div class="flex flex-col gap-5 2xl:mx-auto 2xl:max-w-[2000px]">
+            <div class="flex flex-col gap-5 2xl:mx-auto 2xl:min-w-[75vw] 2xl:max-w-max">
 
                 {{ $slot }}
 
