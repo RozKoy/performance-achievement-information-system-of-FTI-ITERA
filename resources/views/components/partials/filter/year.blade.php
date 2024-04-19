6d27701e2713aeea624872647100a8736ed183bf
RozKoy <okkoy.1401@gmail.com>
2024-04-19 20:38:57 +0700

    feat: text in year filter component

--- resources/views/components/partials/filter/year.blade.php
+++ resources/views/components/partials/filter/year.blade.php
@@ -1,9 +1,12 @@
-<form action="" class="*:rounded-xl *:border *:border-primary/80 *:px-1.5 *:py-0.5 flex snap-x items-center gap-1.5 overflow-x-auto p-2 text-primary max-md:text-sm max-sm:text-xs">
+<form action="" class="*:rounded-xl *:border *:border-primary/80 *:px-1.5 *:py-0.5 flex snap-x items-center gap-1.5 overflow-x-auto whitespace-nowrap p-2 text-primary max-md:text-sm max-sm:text-xs">
     @foreach ($years as $item)
+        @php
+            $temp = \Carbon\Carbon::now()->format('Y') === $item ? ' - NOW' : '';
+        @endphp
         @if ($item === $year)
-            <button name="year" title="{{ $item }}" value="{{ $item }}" class="snap-end border-transparent bg-primary/80 text-white hover:border-primary/80 hover:bg-white hover:text-primary focus:outline-primary" autofocus>{{ $item }}</button>
+            <button name="year" title="{{ $item . $temp }}" value="{{ $item }}" class="snap-end border-transparent bg-primary/80 text-white hover:border-primary/80 hover:bg-white hover:text-primary focus:outline-primary" autofocus>{{ $item . $temp }}</button>
         @else
-            <button name="year" title="{{ $item }}" value="{{ $item }}" class="snap-end hover:border-transparent hover:bg-primary/80 hover:text-white focus:outline-primary">{{ $item }}</button>
+            <button name="year" title="{{ $item . $temp }}" value="{{ $item }}" class="snap-end hover:border-transparent hover:bg-primary/80 hover:text-white focus:outline-primary">{{ $item . $temp }}</button>
         @endif
     @endforeach
 </form>
