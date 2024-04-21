d87b9763650ac8e8c3fc1ada77ba113d7feb70db
RozKoy <okkoy.1401@gmail.com>
2024-04-21 07:58:45 +0700

    feat(action component): text in add button

--- resources/views/components/partials/button/add.blade.php
+++ resources/views/components/partials/button/add.blade.php
@@ -2,7 +2,7 @@
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="aspect-square w-3 sm:w-4">
         <path d="m12 0a12 12 0 1 0 12 12 12.013 12.013 0 0 0 -12-12zm0 22a10 10 0 1 1 10-10 10.011 10.011 0 0 1 -10 10zm1-11h4v2h-4v4h-2v-4h-4v-2h4v-4h2z" />
     </svg>
-    Tambah
+    {{ isset($text) ? $text : 'Tambah' }}
 </button>
 
 @if (!isset($submit) && (isset($href) || isset($route)))
