bca98fb1606e8942664b03b7b882f64a0f25aeb1
RozKoy <okkoy.1401@gmail.com>
2024-04-22 00:49:55 +0700

    feat(view): users management add page

---
+++ resources/views/admin/users/add.blade.php
@@ -0,0 +1,43 @@
+@php
+    $breadCrumbs = [
+        [
+            'link' => 'admin-users',
+            'name' => 'Pengguna',
+        ],
+        [
+            'link' => 'admin-users-add',
+            'name' => 'Tambah',
+        ],
+    ];
+@endphp
+<x-admin-template title="Tambah Pengguna - Teknik Informatika">
+    <x-partials.breadcrumbs.default :$breadCrumbs />
+    <x-partials.heading.h2 text="tambah pengguna" previous="admin-users" />
+    <form action="" class="flex flex-col gap-2">
+        <x-partials.label.default for="name" title="Nama pengguna" text="Nama Pengguna" required />
+        <x-partials.input.text name="name" title="Nama pengguna" autofocus required />
+        <x-partials.label.default for="email" title="Email" text="Email" required />
+        <x-partials.input.text name="email" title="Email" required />
+        <x-partials.label.default for="password" title="Kata sandi" text="Kata Sandi" required />
+        <x-partials.input.text name="password" title="Kata sandi" disabled required />
+        <div id="selection" class="*:rounded-lg *:border *:border-slate-100 *:shadow *:p-1.5 *:gap-1 flex flex-wrap items-center justify-center gap-2 p-2.5 text-primary max-sm:text-sm max-[320px]:text-xs">
+            <div class="flex items-center justify-center">
+                <input type="radio" title="Admin semua akses" name="access" id="editor" value="admin-editor" class="rounded-md border-0 bg-primary/25 checked:bg-primary/80 focus:ring-primary/90" checked required>
+                <label for="editor" title="Admin semua akses">Semua akses</label>
+            </div>
+            <div class="flex items-center justify-center">
+                <input type="radio" title="Admin akses hanya melihat" name="access" id="viewer-admin" value="admin-viewer" class="rounded-md border-0 bg-primary/25 checked:bg-primary/80 focus:ring-primary/90" required>
+                <label for="viewer-admin" title="Admin akses hanya melihat">Hanya melihat</label>
+            </div>
+        </div>
+        <x-partials.button.add submit />
+    </form>
+
+    @pushOnce('script')
+        <script>
+            document.getElementById('name').addEventListener('input', function(event) {
+                document.getElementById('password').value = event.target.value.replaceAll(' ', '_');
+            });
+        </script>
+    @endPushOnce
+</x-admin-template>
