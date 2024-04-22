ccd088f991f2b578a828ececb71faeaed41c2146
RozKoy <okkoy.1401@gmail.com>
2024-04-22 13:25:57 +0700

    feat(component): number type input

---
+++ resources/views/components/partials/input/number.blade.php
@@ -0,0 +1,16 @@
+<input type="number" name="{{ $name }}" id="{{ $name }}" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputNumberCustomMessage(this)" oninput="inputNumberCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))>
+
+@pushOnce('script')
+    <script>
+        function inputNumberCustomMessage(component) {
+            const state = component.validity;
+            const title = component.title;
+
+            if (state.valueMissing) {
+                component.setCustomValidity(title + ' wajib diisi');
+            } else {
+                component.setCustomValidity('');
+            }
+        }
+    </script>
+@endPushOnce
