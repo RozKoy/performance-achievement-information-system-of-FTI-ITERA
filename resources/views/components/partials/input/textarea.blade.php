f71b0be8b185734f2807ecc8cd910f5e30de3d58
RozKoy <okkoy.1401@gmail.com>
2024-04-22 13:22:40 +0700

    feat(js function): invalid custom message

--- resources/views/components/partials/input/textarea.blade.php
+++ resources/views/components/partials/input/textarea.blade.php
@@ -1,1 +1,16 @@
-<textarea name="{{ $name }}" id="{{ $name }}" rows="1" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputTextCustomMessage(this)" oninput="inputTextCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))></textarea>
+<textarea name="{{ $name }}" id="{{ $name }}" rows="1" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputTextareaCustomMessage(this)" oninput="inputTextareaCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))></textarea>
+
+@pushOnce('script')
+    <script>
+        function inputTextareaCustomMessage(component) {
+            const state = component.validity;
+            const title = component.title;
+
+            if (state.valueMissing) {
+                component.setCustomValidity(title + ' wajib diisi');
+            } else {
+                component.setCustomValidity('');
+            }
+        }
+    </script>
+@endPushOnce
