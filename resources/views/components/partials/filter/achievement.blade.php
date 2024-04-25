c157111fb5497cc50c00b6d4481ef5c4a9354480
RozKoy <okkoy.1401@gmail.com>
2024-04-25 13:30:25 +0700

    feat(filter): achievement filter for admin

--- resources/views/components/partials/filter/achievement.blade.php
+++ resources/views/components/partials/filter/achievement.blade.php
@@ -1,4 +1,8 @@
+@php
+    $rs = isset($admin) ? 'admin-history-rs' : 'super-admin-achievement-rs';
+    $iku = isset($admin) ? 'admin-history-iku' : 'super-admin-achievement-iku';
+@endphp
 <div class="*:flex-1 *:rounded-lg *:p-1 *:border-2 *:border-transparent *:bg-primary/80 flex gap-2.5 text-center text-white max-md:text-sm max-sm:text-xs">
-    <a href="{{ url(route('super-admin-achievement-rs')) }}" title="Halaman Rencana Strategis" class="@if (request()->routeIs('super-admin-achievement-rs')) font-semibold outline outline-2 outline-offset-2 outline-primary/80 @endif hover:border-primary/80 hover:bg-white hover:text-primary">Rencana Strategis</a>
-    <a href="{{ url(route('super-admin-achievement-iku')) }}" title="Halaman Indikator Kinerja Utama" class="@if (request()->routeIs('super-admin-achievement-iku')) font-semibold outline outline-2 outline-offset-2 outline-primary/80 @endif hover:border-primary/80 hover:bg-white hover:text-primary">Indikator Kinerja Utama</a>
+    <a href="{{ url(route($rs)) }}" title="Halaman Rencana Strategis" class="@if (request()->routeIs($rs)) font-semibold outline outline-2 outline-offset-2 outline-primary/80 @endif hover:border-primary/80 hover:bg-white hover:text-primary">Rencana Strategis</a>
+    <a href="{{ url(route($iku)) }}" title="Halaman Indikator Kinerja Utama" class="@if (request()->routeIs($iku)) font-semibold outline outline-2 outline-offset-2 outline-primary/80 @endif hover:border-primary/80 hover:bg-white hover:text-primary">Indikator Kinerja Utama</a>
 </div>
