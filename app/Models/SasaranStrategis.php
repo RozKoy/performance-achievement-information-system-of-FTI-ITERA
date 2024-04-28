8094bacab2a615688f323ece08e778a89b7b76b2
RozKoy <okkoy.1401@gmail.com>
2024-04-28 23:18:37 +0700

    feat(model n migration): sasaran strategis in renstra

---
+++ app/Models/SasaranStrategis.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class SasaranStrategis extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'number',
+        'name',
+    ];
+}
---
+++ database/migrations/2024_04_28_153641_create_sasaran_strategis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sasaran_strategis', function (Blueprint $table) {
+            $table->uuid('id');
+
+            $table->unsignedInteger('number');
+            $table->text('name');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sasaran_strategis');
+    }
+};
