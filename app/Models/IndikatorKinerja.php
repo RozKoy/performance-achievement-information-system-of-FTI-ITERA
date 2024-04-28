3e0803f3eadb6756838e0d326dc522acc24e301e
RozKoy <okkoy.1401@gmail.com>
2024-04-28 23:48:42 +0700

    feat(model n migration): indikator kinerja in renstra

---
+++ app/Models/IndikatorKinerja.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class IndikatorKinerja extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $table = 'indikator_kinerja';
+
+    protected $fillable = [
+        'kegiatan_id',
+        'status',
+        'number',
+        'type',
+        'name',
+    ];
+}
---
+++ database/migrations/2024_04_28_163900_create_indikator_kinerjas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('indikator_kinerja', function (Blueprint $table) {
+            $table->uuid('id');
+
+            $table->unsignedInteger('number');
+            $table->string('status', 11);
+            $table->string('type', 6);
+            $table->text('name');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->primary('id');
+
+            $table->foreignUuid('kegiatan_id')->constrained('kegiatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('indikator_kinerja');
+    }
+};
