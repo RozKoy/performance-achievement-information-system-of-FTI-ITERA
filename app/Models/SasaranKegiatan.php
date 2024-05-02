71cec519cde4581118e209071fa85d4f741cb754
RozKoy <okkoy.1401@gmail.com>
2024-05-02 17:50:25 +0700

    feat(model): sasaran kegiatan

---
+++ app/Models/SasaranKegiatan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class SasaranKegiatan extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $table = 'sasaran_kegiatan';
+
+    protected $fillable = [
+        'number',
+        'name',
+    ];
+}
