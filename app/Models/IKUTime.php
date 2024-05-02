a92d09a63f60e84074e8ffe4995d40a85d0815b8
RozKoy <okkoy.1401@gmail.com>
2024-05-02 20:42:48 +0700

    style: sort uses module

--- app/Models/IKUTime.php
+++ app/Models/IKUTime.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
 
 class IKUTime extends Model
 {
