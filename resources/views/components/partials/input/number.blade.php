a12376b314744a3ad96176d72c250ea165382a57
RozKoy <okkoy.1401@gmail.com>
2024-05-20 08:36:05 +0700

    feat(number input component): error message

--- resources/views/components/partials/input/number.blade.php
+++ resources/views/components/partials/input/number.blade.php
@@ -1,4 +1,10 @@
-<input type="number" name="{{ $name }}" id="{{ $name }}" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputNumberCustomMessage(this)" oninput="inputNumberCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))>
+<div class="flex flex-col gap-0.5">
+    <input type="number" name="{{ $name }}" id="{{ $name }}" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputNumberCustomMessage(this)" oninput="inputNumberCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))>
+
+    @error($name)
+        <p class="text-xs text-red-500 lg:text-sm">{{ $message }}</p>
+    @enderror
+</div>
 
 @pushOnce('script')
     <script>
