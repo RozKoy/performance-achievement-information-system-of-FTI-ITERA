cc34a8cda82ef91b7582d7be264665da320f9e3d
RozKoy <okkoy.1401@gmail.com>
2024-05-24 22:48:39 +0700

    feat(migration n model): rencana strategis evaluation

---
+++ app/Models/RSEvaluation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class RSEvaluation extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $table = 'rs_evaluations';
+
+    protected $fillable = [
+        'evaluation',
+        'follow_up',
+        'status',
+        'target',
+
+        'indikator_kinerja_id',
+    ];
+}
---
+++ database/migrations/2024_05_24_153220_create_r_s_evaluations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rs_evaluations', function (Blueprint $table) {
+            $table->uuid('id');
+
+            $table->string('evaluation')->nullable();
+            $table->string('follow_up')->nullable();
+            $table->boolean('status');
+            $table->string('target');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->primary('id');
+
+            $table->foreignUuid('indikator_kinerja_id')->constrained('indikator_kinerja');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rs_evaluations');
+    }
+};
