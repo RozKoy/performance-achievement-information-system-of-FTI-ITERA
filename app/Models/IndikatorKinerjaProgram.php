7113e37db74a18a6b8a9c9bd414ac41848d82f98
RozKoy <okkoy.1401@gmail.com>
2024-05-27 18:59:05 +0700

    feat(model relation): indikator kinerja program with columns

--- app/Models/IndikatorKinerjaProgram.php
+++ app/Models/IndikatorKinerjaProgram.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
@@ -26,5 +27,10 @@ class IndikatorKinerjaProgram extends Model
     public function programStrategis(): BelongsTo
     {
         return $this->belongsTo(ProgramStrategis::class);
+    }
+
+    public function columns(): HasMany
+    {
+        return $this->hasMany(IKPColumn::class);
     }
 }
