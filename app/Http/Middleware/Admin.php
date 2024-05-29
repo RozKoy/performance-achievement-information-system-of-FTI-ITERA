9ee92cd0bd253980a2220d0a7b01fbd677db5e47
RozKoy <okkoy.1401@gmail.com>
2024-05-29 20:28:00 +0700

    feat(middleware): super admin n admin

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check()) {
+            if (auth()->user()->role === 'admin' && auth()->user()->unit()->exists()) {
+                return $next($request);
+            }
+        }
+
+        return redirect()->route('login');
+    }
+}
