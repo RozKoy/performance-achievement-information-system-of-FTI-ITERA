195576913ad9ba996b7f27c4679a8406579ffffa
RozKoy <okkoy.1401@gmail.com>
2024-06-09 10:13:00 +0700

    feat(model n migration): iku evaluation

---
+++ app/Models/IKUEvaluation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class IKUEvaluation extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $table = 'iku_evaluations';
+
+    protected $fillable = [
+        'evaluation',
+        'follow_up',
+        'status',
+        'target',
+
+        'indikator_kinerja_program_id',
+    ];
+}
---
+++ database/migrations/2024_06_09_030736_create_i_k_u_evaluations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('iku_evaluations', function (Blueprint $table) {
+            $table->uuid('id');
+
+            $table->string('evaluation')->nullable();
+            $table->string('follow_up')->nullable();
+            $table->boolean('status');
+            $table->integer('target');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreignUuid('indikator_kinerja_program_id')->constrained('indikator_kinerja_program');
+
+            $table->primary('id');
+            $table->unique('indikator_kinerja_program_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('iku_evaluations');
+    }
+};
