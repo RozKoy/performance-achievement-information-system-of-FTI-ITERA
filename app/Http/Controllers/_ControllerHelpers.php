c068879a8c42abb5b47dc5b555f73f130084c5c3
RozKoy <okkoy.1401@gmail.com>
2024-10-15 14:41:21 +0700

    style: controllerHelpers

--- app/Http/Controllers/_ControllerHelpers.php
+++ app/Http/Controllers/_ControllerHelpers.php
@@ -6,11 +6,21 @@ use Illuminate\Http\RedirectResponse;
 
 class _ControllerHelpers
 {
+    /**
+     * BackWithInputWithErrors
+     * @param string|array $errors
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public static function BackWithInputWithErrors(string|array $errors): RedirectResponse
     {
         return back()->withInput()->withErrors($errors);
     }
 
+    /**
+     * RedirectWithRoute
+     * @param string $route
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public static function RedirectWithRoute(string $route): RedirectResponse
     {
         return redirect()->route($route);
