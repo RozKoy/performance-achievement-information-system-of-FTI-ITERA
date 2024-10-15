ae13dbb6a8d47ec8613da4a332754ff494af9271
RozKoy <okkoy.1401@gmail.com>
2024-10-15 14:01:00 +0700

    feat(h2 partial component): tooltip button

--- resources/views/components/partials/heading/h2.blade.php
+++ resources/views/components/partials/heading/h2.blade.php
@@ -10,5 +10,27 @@
         </a>
     @endif
 
-    <h2 title="Halaman {{ $text }}" class="text-xl font-semibold capitalize text-primary sm:text-2xl">{{ $text }}</h2>
+    <h2 title="Halaman {{ $text }}" class="text-xl font-semibold capitalize text-primary sm:text-2xl">
+        {{ $text }}
+
+        @if ($tooltip ?? false)
+            <button title="Bantuan" data-popover-target="popover-description" data-popover-placement="bottom-start" data-popover-trigger="click" type="button">
+                <svg class="aspect-square w-5 text-gray-400 hover:text-gray-500 max-sm:w-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                </svg>
+                <span class="sr-only">Show information</span>
+            </button>
+        @endif
+
+    </h2>
+
+    @if ($tooltip ?? false)
+        <div data-popover id="popover-description" role="tooltip" class="invisible absolute z-50 inline-block w-96 max-w-full rounded-lg border border-primary bg-white text-sm text-gray-500 opacity-0 shadow-sm transition-opacity duration-300 max-sm:text-xs">
+            <div class="space-y-2 p-3">
+                {{ $slot }}
+            </div>
+            {{-- <div data-popper-arrow></div> --}}
+        </div>
+    @endif
+
 </div>
