9461d423b753caf48695f75179116b15007e4509
RozKoy <okkoy.1401@gmail.com>
2024-10-15 22:39:46 +0700

    feat(manage button component): viewonly mode

--- resources/views/components/partials/button/manage.blade.php
+++ resources/views/components/partials/button/manage.blade.php
@@ -1,5 +1,5 @@
-<a href="{{ url($link) }}" title="Kelola" class="rounded-full bg-gray-500 p-0.5 text-white hover:bg-gray-400">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="aspect-square w-5 sm:w-6">
+<a href="{{ url($link) }}" title="Kelola" class="{{ isset($viewOnly) ? 'pointer-events-none' : 'hover:bg-gray-400' }} rounded-full bg-gray-500 p-0.5 text-white">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="{{ isset($viewOnly) ? '' : 'sm:w-6' }} aspect-square w-5">
         <path d="m12,0C5.383,0,0,5.383,0,12s5.383,12,12,12,12-5.383,12-12S18.617,0,12,0Zm0,22c-5.514,0-10-4.486-10-10S6.486,2,12,2s10,4.486,10,10-4.486,10-10,10Zm-4-10c0,.828-.672,1.5-1.5,1.5s-1.5-.672-1.5-1.5.672-1.5,1.5-1.5,1.5.672,1.5,1.5Zm11,0c0,.828-.672,1.5-1.5,1.5s-1.5-.672-1.5-1.5.672-1.5,1.5-1.5,1.5.672,1.5,1.5Zm-5.5,0c0,.828-.672,1.5-1.5,1.5s-1.5-.672-1.5-1.5.672-1.5,1.5-1.5,1.5.672,1.5,1.5Z" />
     </svg>
 </a>
