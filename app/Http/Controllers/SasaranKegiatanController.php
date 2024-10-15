99cf433b8cadcb574cc93e6871931b918a16df8c
RozKoy <okkoy.1401@gmail.com>
2024-10-15 22:55:18 +0700

    style: sasaran kegiatan controller

--- app/Http/Controllers/SasaranKegiatanController.php
+++ app/Http/Controllers/SasaranKegiatanController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SasaranKegiatan\EditRequest;
 use App\Http\Requests\SasaranKegiatan\AddRequest;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\View\View;
 use App\Models\SasaranKegiatan;
 use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
@@ -12,7 +15,12 @@ use App\Models\IKUYear;
 
 class SasaranKegiatanController extends Controller
 {
-    public function homeView(Request $request)
+    /**
+     * Sasaran kegiatan home view
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function homeView(Request $request): Factory|View
     {
         $time = IKUYear::currentTime();
 
@@ -36,7 +44,11 @@ class SasaranKegiatanController extends Controller
         return view('super-admin.iku.sk.home', compact('data'));
     }
 
-    public function addView()
+    /**
+     * Sasaran kegiatan add view
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function addView(): Factory|View
     {
         $time = IKUYear::currentTime();
 
@@ -57,16 +69,19 @@ class SasaranKegiatanController extends Controller
         return view('super-admin.iku.sk.add', compact('data'));
     }
 
-    public function add(AddRequest $request)
+    /**
+     * Sasaran kegiatan add function
+     * @param \App\Http\Requests\SasaranKegiatan\AddRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function add(AddRequest $request): RedirectResponse
     {
         $time = IKUYear::currentTime();
 
         $number = (int) $request['number'];
         $dataCount = $time->sasaranKegiatan->count();
         if ($number > $dataCount + 1) {
-            return back()
-                ->withInput()
-                ->withErrors(['number' => 'Nomor tidak sesuai dengan jumlah data']);
+            return _ControllerHelpers::BackWithInputWithErrors(['number' => 'Nomor tidak sesuai dengan jumlah data']);
         }
 
         if ($number <= $dataCount) {
@@ -80,10 +95,15 @@ class SasaranKegiatanController extends Controller
         $sk->time()->associate($time);
         $sk->save();
 
-        return redirect()->route('super-admin-iku-sk');
+        return _ControllerHelpers::RedirectWithRoute('super-admin-iku-sk');
     }
 
-    public function editView(SasaranKegiatan $sk)
+    /**
+     * Sasaran kegiatan edit view
+     * @param \App\Models\SasaranKegiatan $sk
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function editView(SasaranKegiatan $sk): Factory|View
     {
         $count = $sk->time->sasaranKegiatan->count();
 
@@ -119,15 +139,19 @@ class SasaranKegiatanController extends Controller
         ]));
     }
 
-    public function edit(EditRequest $request, SasaranKegiatan $sk)
+    /**
+     * Sasaran kegiatan edit function
+     * @param \App\Http\Requests\SasaranKegiatan\EditRequest $request
+     * @param \App\Models\SasaranKegiatan $sk
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function edit(EditRequest $request, SasaranKegiatan $sk): RedirectResponse
     {
         $time = $sk->time;
 
         $number = (int) $request['number'];
         if ($number > $time->sasaranKegiatan->count()) {
-            return back()
-                ->withInput()
-                ->withErrors(['number' => 'Nomor tidak sesuai dengan jumlah data']);
+            return _ControllerHelpers::BackWithInputWithErrors(['number' => 'Nomor tidak sesuai dengan jumlah data']);
         }
 
         $currentNumber = $sk->number;
@@ -151,15 +175,20 @@ class SasaranKegiatanController extends Controller
         $sk->save();
 
         if ($time->year === Carbon::now()->format('Y')) {
-            return redirect()->route('super-admin-iku-sk');
+            return _ControllerHelpers::RedirectWithRoute('super-admin-iku-sk');
         } else {
-            return redirect()->route('super-admin-achievement-iku', [
+            return _ControllerHelpers::RedirectWithRoute('super-admin-achievement-iku', [
                 'year' => $time->year
             ]);
         }
     }
 
-    public function delete(SasaranKegiatan $sk)
+    /**
+     * Sasaran kegiatan delete function
+     * @param \App\Models\SasaranKegiatan $sk
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete(SasaranKegiatan $sk): RedirectResponse
     {
         $sk = SasaranKegiatan::currentOrFail($sk->id);
 
