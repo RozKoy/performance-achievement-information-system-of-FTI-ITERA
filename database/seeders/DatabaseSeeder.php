c9ce1160ae94a4683f06824279be991c7dc01b9f
RozKoy <okkoy.1401@gmail.com>
2024-10-21 22:35:06 +0700

    feat(database seeder): call unit n user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
+        $this->call([
+            UnitSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
