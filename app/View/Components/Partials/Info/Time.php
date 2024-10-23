6bdca3ae24c96def9d823f4fd00baab3824ccd71
RozKoy <okkoy.1401@gmail.com>
2024-10-23 20:25:26 +0700

    feat(component): time info component

---
+++ app/View/Components/Partials/Info/Time.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\View\Components\Partials\Info;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Carbon;
+use Illuminate\View\Component;
+use Closure;
+
+class Time extends Component
+{
+	/**
+	 * Create a new component instance.
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Get the view / contents that represent the component.
+	 */
+	public function render(): View|Closure|string
+	{
+		$currentMonth = (int) Carbon::now()->format('m');
+		$currentYear = Carbon::now()->format('Y');
+
+		// Rencana Strategis
+		$rsPeriods = ['Januari - Juni', 'Juli - Desember'];
+		$rsIndex = $currentMonth <= 6 ? 0 : 1;
+
+		// Indikator Kinerja Utama
+		$ikuPeriods = ['TW 1 | Jan - Mar', 'TW 2 | Apr - Jun', 'TW 3 | Jul - Sep', 'TW 4 | Okt - Des'];
+		$ikuIndex = 0;
+		foreach ([3, 6, 9, 12] as $key => $value) {
+			if ($currentMonth <= $value) {
+				$ikuIndex = $key;
+				break;
+			}
+		}
+
+		$iku = [$ikuPeriods[$ikuIndex], $currentYear];
+		$rs = [$rsPeriods[$rsIndex], $currentYear];
+
+		return view('components.partials.info.time', compact([
+			'iku',
+			'rs',
+		]));
+	}
+}
