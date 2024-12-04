fd82cda66a3e7ac0e9c451d8470bd827c1aba3c1
RozKoy <okkoy.1401@gmail.com>
2024-12-04 20:42:28 +0700

    feat(iku unit status model): belongs to relation with ikp n unit

--- app/Models/IKUUnitStatus.php
+++ app/Models/IKUUnitStatus.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
@@ -19,4 +20,21 @@ class IKUUnitStatus extends Model
         'indikator_kinerja_program_id',
         'unit_id',
     ];
+
+
+    /*
+    | -----------------------------------------------------------------
+    | RELATION - BELONGSTO
+    | -----------------------------------------------------------------
+    */
+
+    public function indikatorKinerjaProgram(): BelongsTo
+    {
+        return $this->belongsTo(IndikatorKinerjaProgram::class);
+    }
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
 }
