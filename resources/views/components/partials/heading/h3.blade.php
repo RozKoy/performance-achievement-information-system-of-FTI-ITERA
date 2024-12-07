e84982773ad9cca0018827fbcf45d1ababbb39ae
RozKoy <okkoy.1401@gmail.com>
2024-12-07 19:27:07 +0700

    style: third heading view component

--- resources/views/components/partials/heading/h3.blade.php
+++ resources/views/components/partials/heading/h3.blade.php
@@ -1,3 +1,11 @@
+@php
+    /**
+     * @required @param dataNumber: string
+     * @required @param dataText: string
+     * @required @param title: string
+     */
+@endphp
+
 <div class="flex flex-wrap items-center gap-3">
     <h3 title="{{ $title }}" class="text-lg font-semibold capitalize text-primary sm:text-xl">{{ $title }} :</h3>
     <div class="*:px-2.5 *:py-1 *:truncate flex min-w-[60%] flex-1 items-center overflow-hidden rounded-lg border border-slate-200 text-primary max-sm:text-sm">
