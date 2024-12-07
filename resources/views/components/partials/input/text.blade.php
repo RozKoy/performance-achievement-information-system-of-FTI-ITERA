de7acb9a401c6488a7df1b62ab17d9b15b3e5c37
RozKoy <okkoy.1401@gmail.com>
2024-12-07 21:05:31 +0700

    style: text input view component

--- resources/views/components/partials/input/text.blade.php
+++ resources/views/components/partials/input/text.blade.php
@@ -1,11 +1,24 @@
+@php
+    /**
+     * @required @param title: string
+     * @required @param name: string
+     * @optional @param autofocus: mixed
+     * @optional @param oldValue: string
+     * @optional @param disabled: mixed
+     * @optional @param required: mixed
+     * @optional @param onblur: string
+     * @optional @param style: string
+     * @optional @param value: string
+     */
+@endphp
+
 <div class="flex flex-col gap-0.5">
-    <input type="text" name="{{ $name }}" id="{{ $name }}" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputTextCustomMessage(this)" oninput="inputTextCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($oldvalue) oldvalue="{{ $oldvalue }}" @endisset @isset($onblur) onblur="{{ $onblur }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))>
+    <input type="text" name="{{ $name }}" id="{{ $name }}" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputTextCustomMessage(this)" oninput="inputTextCustomMessage(this)" class="{{ $style ?? '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($oldvalue) oldvalue="{{ $oldvalue }}" @endisset @isset($onblur) onblur="{{ $onblur }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))>
 
     @error($name)
         <p class="text-xs text-red-500 lg:text-sm">{{ $message }}</p>
     @enderror
 </div>
-
 
 @pushOnce('script')
     <script>
