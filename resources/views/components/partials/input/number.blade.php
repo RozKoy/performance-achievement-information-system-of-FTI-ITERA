83b899de2754dd89b68379615b2c8c3a5c896133
RozKoy <okkoy.1401@gmail.com>
2024-12-07 20:24:30 +0700

    style: number input view component

--- resources/views/components/partials/input/number.blade.php
+++ resources/views/components/partials/input/number.blade.php
@@ -1,3 +1,15 @@
+@php
+    /**
+     * @required @param title: string
+     * @required @param name: string
+     * @optional @param autofocus: mixed
+     * @optional @param disabled: mixed
+     * @optional @param required: mixed
+     * @optional @param style: string
+     * @optional @param value: string
+     */
+@endphp
+
 <div class="flex flex-col gap-0.5">
     <input type="number" name="{{ $name }}" id="{{ $name }}" placeholder="Masukkan {{ strtolower($title) }}" title="{{ ucfirst($title) }}" oninvalid="inputNumberCustomMessage(this)" oninput="inputNumberCustomMessage(this)" class="{{ isset($style) ? $style : '' }} rounded-lg border-2 !border-slate-100 !px-2 !py-1.5 text-primary focus:!border-primary focus:!outline-none focus:!ring-0 disabled:cursor-not-allowed disabled:bg-primary/10 max-sm:text-sm" @isset($value) value="{{ $value }}" @endisset @isset($autofocus) autofocus @endisset @disabled(isset($disabled)) @required(isset($required))>
 
