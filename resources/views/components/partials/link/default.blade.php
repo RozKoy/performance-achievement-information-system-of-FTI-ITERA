571ad7ae1212d286ba2885bdb8feaeed964fc24e
RozKoy <okkoy.1401@gmail.com>
2024-12-07 21:11:25 +0700

    style: default link view component

--- resources/views/components/partials/link/default.blade.php
+++ resources/views/components/partials/link/default.blade.php
@@ -1,1 +1,14 @@
-<a href="{{ url(route($route)) }}" title="Halaman {{ $title }}" class="{{ isset($left) ? 'self-start' : '' }} {{ isset($center) ? 'self-center' : '' }} {{ isset($right) ? 'self-end' : '' }} w-fit capitalize text-primary underline hover:text-primary/90">{{ $name }}</a>
+@php
+    /**
+     * @required @param route: string
+     * @required @param title: string
+     * @required @param name: string
+     * @optional @param center: mixed
+     * @optional @param right: mixed
+     * @optional @param left: mixed
+     */
+@endphp
+
+<a href="{{ url(route($route)) }}" title="Halaman {{ $title }}" class="{{ isset($left) ? 'self-start' : '' }} {{ isset($center) ? 'self-center' : '' }} {{ isset($right) ? 'self-end' : '' }} w-fit capitalize text-primary underline hover:text-primary/90">
+    {{ $name }}
+</a>
