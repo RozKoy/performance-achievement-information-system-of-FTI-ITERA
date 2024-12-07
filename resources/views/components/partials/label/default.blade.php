07624ef75a4467a7c5fc7cc1c8a9453f60afbe58
RozKoy <okkoy.1401@gmail.com>
2024-12-07 21:09:26 +0700

    style: default label view component

--- resources/views/components/partials/label/default.blade.php
+++ resources/views/components/partials/label/default.blade.php
@@ -1,1 +1,11 @@
-<label for="{{ $for }}" title="{{ ucfirst($title) }}" class="@isset($required) after:content-['*'] after:text-red-500 @endisset max-sm:text-sm">{{ ucwords($title) }}</label>
+@php
+    /**
+     * @required @param title: string
+     * @required @param for: string
+     * @optional @param required: mixed
+     */
+@endphp
+
+<label for="{{ $for }}" title="{{ ucfirst($title) }}" class="@isset($required) after:content-['*'] after:text-red-500 @endisset max-sm:text-sm">
+    {{ ucwords($title) }}
+</label>
