bbb364bf8d12a5714f4081411e48c43f92d8a14a
RozKoy <okkoy.1401@gmail.com>
2024-12-07 19:13:04 +0700

    style: submit button view component

--- resources/views/components/partials/button/submit.blade.php
+++ resources/views/components/partials/button/submit.blade.php
@@ -1,1 +1,9 @@
-<button type="submit" title="Tombol {{ $title }}" class="rounded-lg bg-primary py-2 capitalize text-white hover:bg-primary/90 focus:ring-2 focus:ring-primary/90">{{ $title }}</button>
+@php
+    /**
+     * @required @param title: string
+     */
+@endphp
+
+<button type="submit" title="Tombol {{ $title }}" class="rounded-lg bg-primary py-2 capitalize text-white hover:bg-primary/90 focus:ring-2 focus:ring-primary/90">
+    {{ $title }}
+</button>
