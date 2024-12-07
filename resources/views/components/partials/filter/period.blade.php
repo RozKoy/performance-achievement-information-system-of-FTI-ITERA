8ff89884b1ecd22762299fc0ffbaca8b2b7935c3
RozKoy <okkoy.1401@gmail.com>
2024-12-07 19:19:33 +0700

    style: period filter view component

--- resources/views/components/partials/filter/period.blade.php
+++ resources/views/components/partials/filter/period.blade.php
@@ -1,3 +1,15 @@
+@php
+    /**
+     * @type Type [
+     *      @required title: string
+     *      @required value: string
+     * ]
+     *
+     * @required @param periods: Type[]
+     * @required @param period: string
+     */
+@endphp
+
 <form class="*:whitespace-nowrap *:flex-1 *:border *:border-primary/80 *:rounded-xl *:py-0.5 *:px-1 flex flex-wrap gap-2 text-primary max-md:text-sm max-sm:text-xs">
 
     @if (request()->query('year') !== null)
@@ -5,7 +17,9 @@
     @endif
 
     @foreach ($periods as $item)
-        <button title="{{ $item['title'] }}" name="period" value="{{ $item['value'] }}" class="{{ $item['value'] === $period ? 'outline outline-1 outline-offset-2 outline-primary/80 hover:border-transparent hover:bg-primary/80 hover:text-white' : 'hover:border-transparent hover:bg-primary/80 hover:text-white' }}">{{ $item['title'] }}</button>
+        <button title="{{ $item['title'] }}" name="period" value="{{ $item['value'] }}" class="{{ $item['value'] === $period ? 'outline outline-1 outline-offset-2 outline-primary/80 hover:border-transparent hover:bg-primary/80 hover:text-white' : 'hover:border-transparent hover:bg-primary/80 hover:text-white' }}">
+            {{ $item['title'] }}
+        </button>
     @endforeach
 
 </form>
