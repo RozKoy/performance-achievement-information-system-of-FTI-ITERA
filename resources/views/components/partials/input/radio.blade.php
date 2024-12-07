7305a946eafdeaf237cd1c314f3bb1c977193804
RozKoy <okkoy.1401@gmail.com>
2024-12-07 20:29:15 +0700

    style: radio input view component

--- resources/views/components/partials/input/radio.blade.php
+++ resources/views/components/partials/input/radio.blade.php
@@ -1,1 +1,12 @@
+@php
+    /**
+     * @required @param title: string
+     * @required @param value: string
+     * @required @param name: string
+     * @required @param id: string
+     * @optional @param required: mixed
+     * @optional @param checked: mixed
+     */
+@endphp
+
 <input type="radio" title="{{ $title }}" name="{{ $name }}" id="{{ $id }}" value="{{ $value }}" oninvalid="this.setCustomValidity('Mohon pilih salah satu')" onchange="this.setCustomValidity('')" class="rounded-md border-0 bg-primary/25 checked:bg-primary/80 focus:ring-primary/90" @isset($checked) checked @endisset @isset($required) required @endisset>
