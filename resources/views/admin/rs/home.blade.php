59272f203fa7a904ec867eacfbd7dc11bf72fd58
RozKoy <okkoy.1401@gmail.com>
2024-12-08 23:36:03 +0700

    feat n style(admin rs home view): particular column n styled

--- resources/views/admin/rs/home.blade.php
+++ resources/views/admin/rs/home.blade.php
@@ -6,24 +6,18 @@
         ],
     ];
 @endphp
+
 <x-admin-template title="Renstra - Capaian Kinerja - {{ auth()->user()->unit->name }}">
-    <x-partials.breadcrumbs.default :$breadCrumbs admin />
+    <x-partials.breadcrumbs.default :$breadCrumbs />
 
     @if (count($years))
         <x-partials.filter.year :$years :$year />
         <x-partials.filter.period :$periods :$period />
         <x-partials.heading.h2 text="capaian kinerja - rencana strategis" />
+
         <div class="flex gap-1.5 max-lg:flex-wrap sm:gap-3">
             <form action="" class="mr-auto">
-
-                @if (request()->query('year'))
-                    <input type="hidden" name="year" value="{{ request()->query('year') }}">
-                @endif
-
-                @if (request()->query('period'))
-                    <input type="hidden" name="period" value="{{ request()->query('period') }}">
-                @endif
-
+                <x-functions.query-handler :data="['year', 'period']" />
                 <x-partials.input.select onchange="this.form.submit()" name="status" title="Filter status" :data="$status" />
             </form>
             <x-partials.badge.time :data="$badge" />
@@ -37,9 +31,25 @@
             <table class="min-w-full max-lg:text-sm max-md:text-xs">
                 <thead>
                     <tr class="*:font-normal *:px-5 *:py-2.5 *:whitespace-nowrap divide-x bg-primary/80 text-white">
-                        <th title="Nomor">No</th>
-                        <th title="Sasaran strategis">Sasaran Strategis</th>
-                        <th title="Kegiatan">Kegiatan</th>
+
+                        @if (request()->query('ss') === 'show')
+                            <th title="Nomor">No</th>
+                        @endif
+
+                        <th title="{{ request()->query('ss') === 'show' ? 'Sasaran strategis' : 'Tampilkan sasaran strategis?' }}">
+                            <form action="" method="GET" class="inline">
+                                <x-functions.query-handler :data="['year', 'period', 'status', 'k']" />
+                                <input type="checkbox" name="ss" title="Tampilkan sasaran strategis?" onchange="this.form.submit()" value="{{ request()->query('ss') !== null ? '' : 'show' }}" class="rounded border-2 border-white text-primary checked:outline-primary focus:outline-primary disabled:border-slate-300" @checked(request()->query('ss') === 'show')>
+                            </form>
+                            {{ request()->query('ss') === 'show' ? 'Sasaran Strategis' : 'SS' }}
+                        </th>
+                        <th title="{{ request()->query('k') === 'show' ? 'Kegiatan' : 'Tampilkan kegiatan?' }}">
+                            <form action="" method="GET" class="inline">
+                                <x-functions.query-handler :data="['year', 'period', 'status', 'ss']" />
+                                <input type="checkbox" name="k" title="Tampilkan kegiatan?" onchange="this.form.submit()" value="{{ request()->query('k') !== null ? '' : 'show' }}" class="rounded border-2 border-white text-primary checked:outline-primary focus:outline-primary disabled:border-slate-300" @checked(request()->query('k') === 'show')>
+                            </form>
+                            {{ request()->query('k') === 'show' ? 'Kegiatan' : 'K' }}
+                        </th>
                         <th title="Indikator kinerja">Indikator Kinerja</th>
                         <th title="Target {{ $year }}">Target {{ $year }}</th>
                         <th title="Realisasi {{ $year }}">Realisasi {{ $year }}</th>
@@ -55,21 +65,35 @@
 
                                     @if ($loop->iteration === 1)
                                         @if ($loop->parent->iteration === 1)
-                                            <td title="{{ $ss['number'] }}" rowspan="{{ $ss['rowspan'] }}">{{ $ss['number'] }}</td>
+                                            @if (request()->query('ss') === 'show')
+                                                <td title="{{ $ss['number'] }}" rowspan="{{ $ss['rowspan'] }}">
+                                                    {{ $ss['number'] }}
+                                                </td>
+                                            @endif
 
-                                            <td title="{{ $ss['ss'] }}" rowspan="{{ $ss['rowspan'] }}" class="min-w-72 w-max text-left">{{ $ss['ss'] }}</td>
+                                            <td title="{{ request()->query('ss') === 'show' ? $ss['ss'] : '' }}" rowspan="{{ $ss['rowspan'] }}" class="{{ request()->query('ss') === 'show' ? 'min-w-72' : '' }} w-max text-left">
+                                                {{ request()->query('ss') === 'show' ? $ss['ss'] : '' }}
+                                            </td>
                                         @endif
 
-                                        <td title="{{ $k['k'] }}" rowspan="{{ $k['rowspan'] }}" class="min-w-72 w-max text-left">{{ $k['k'] }}</td>
+                                        <td title="{{ request()->query('k') === 'show' ? $k['k'] : '' }}" rowspan="{{ $k['rowspan'] }}" class="{{ request()->query('k') === 'show' ? 'min-w-72' : '' }} w-max text-left">
+                                            {{ request()->query('k') === 'show' ? $k['k'] : '' }}
+                                        </td>
                                     @endif
 
                                     <td title="{{ $ik['ik'] }}" class="min-w-72 group relative z-10 w-max text-left">
                                         {{ $ik['ik'] }}
-                                        <span title="{{ $ik['type'] }}" class="absolute bottom-1.5 right-1.5 cursor-default rounded-lg bg-primary/25 p-1 text-xs uppercase text-primary/75">{{ $ik['type'] }}</span>
+                                        <span title="{{ $ik['type'] }}" class="absolute bottom-1.5 right-1.5 cursor-default rounded-lg bg-primary/25 p-1 text-xs uppercase text-primary/75">
+                                            {{ $ik['type'] }}
+                                        </span>
                                     </td>
 
-                                    <td title="{{ $ik['target'] }}{{ $ik['type'] === 'persen' && $ik['target'] !== null ? '%' : '' }}">{{ $ik['target'] }}{{ $ik['type'] === 'persen' && $ik['target'] !== null ? '%' : '' }}</td>
-                                    <td title="{{ $ik['yearRealization'] }}{{ $ik['type'] === 'persen' && $ik['yearRealization'] !== null ? '%' : '' }}">{{ $ik['yearRealization'] }}{{ $ik['type'] === 'persen' && $ik['yearRealization'] !== null ? '%' : '' }}</td>
+                                    <td title="{{ $ik['target'] }}{{ $ik['type'] === 'persen' && $ik['target'] !== null ? '%' : '' }}">
+                                        {{ $ik['target'] }}{{ $ik['type'] === 'persen' && $ik['target'] !== null ? '%' : '' }}
+                                    </td>
+                                    <td title="{{ $ik['yearRealization'] }}{{ $ik['type'] === 'persen' && $ik['yearRealization'] !== null ? '%' : '' }}">
+                                        {{ $ik['yearRealization'] }}{{ $ik['type'] === 'persen' && $ik['yearRealization'] !== null ? '%' : '' }}
+                                    </td>
 
                                     <td>
                                         @if (isset($ik['realization']))
@@ -78,11 +102,13 @@
                                             @endphp
 
                                             <div id="realization-{{ $id }}" class="group relative z-10 flex items-center justify-center gap-1 py-1.5">
-                                                <p title="{{ $ik['realization'] }}{{ $ik['type'] === 'persen' && $ik['realization'] !== null ? '%' : '' }}">{{ $ik['realization'] }}{{ $ik['type'] === 'persen' && $ik['realization'] !== null ? '%' : '' }}</p>
+                                                <p title="{{ $ik['realization'] }}{{ $ik['type'] === 'persen' && $ik['realization'] !== null ? '%' : '' }}">
+                                                    {{ $ik['realization'] }}{{ $ik['type'] === 'persen' && $ik['realization'] !== null ? '%' : '' }}
+                                                </p>
                                                 <a href="{{ $ik['link'] }}" title="Link Bukti" target="__blank" class="text-primary underline">Link Bukti</a>
 
                                                 @if (auth()->user()->access === 'editor')
-                                                    <x-partials.button.edit button onclick="toggleEditForm('{{ $id }}')" style="absolute hidden top-0.5 right-0.5 group-hover:block group-focus:block" />
+                                                    <x-partials.button.edit onclick="toggleEditForm('{{ $id }}')" style="absolute hidden top-0.5 right-0.5 group-hover:block group-focus:block" button />
                                                 @endif
 
                                             </div>
@@ -90,10 +116,8 @@
                                             @if (auth()->user()->access === 'editor')
                                                 <form id="form-realization-{{ $id }}" action="{{ route('admin-rs-add', ['period' => $periodId, 'ik' => $ik['id']]) }}" method="POST" class="hidden flex-col gap-0.5">
                                                     @csrf
-                                                    <div class="flex-1">
+                                                    <div class="*:w-full flex items-start justify-center max-lg:flex-wrap">
                                                         <x-partials.input.text name="realization-{{ $ik['id'] }}" title="realisasi ({{ $ik['type'] }})" value="{{ $ik['realization'] }}" />
-                                                    </div>
-                                                    <div class="flex-1">
                                                         <x-partials.input.text name="link-{{ $ik['id'] }}" title="link bukti" value="{{ $ik['link'] }}" required />
                                                     </div>
                                                     <div class="ml-auto flex items-center justify-end gap-0.5">
@@ -104,15 +128,13 @@
                                             @endif
                                         @else
                                             @if (auth()->user()->access === 'editor')
-                                                <form action="{{ route('admin-rs-add', ['period' => $periodId, 'ik' => $ik['id']]) }}" method="POST" class="flex items-center gap-1">
+                                                <form action="{{ route('admin-rs-add', ['period' => $periodId, 'ik' => $ik['id']]) }}" method="POST" class="flex flex-wrap items-center gap-1">
                                                     @csrf
-                                                    <div class="flex-1">
+                                                    <div class="*:w-full flex items-start justify-center max-lg:flex-wrap">
                                                         <x-partials.input.text name="realization-{{ $ik['id'] }}" title="realisasi ({{ $ik['type'] }})" required />
-                                                    </div>
-                                                    <div class="flex-1">
                                                         <x-partials.input.text name="link-{{ $ik['id'] }}" title="link bukti" required />
                                                     </div>
-                                                    <x-partials.button.add text="" submit />
+                                                    <x-partials.button.add text="" style="ml-auto" submit />
                                                 </form>
                                             @endif
                                         @endif
@@ -134,6 +156,10 @@
         @if (!count($data) && request()->query('status'))
             <p class="text-center text-red-500 max-lg:text-sm max-md:text-xs">Status : {{ request()->query('status') === 'done' ? 'Sudah diisi' : 'Belum diisi' }}<br>Data capaian kinerja tidak dapat ditemukan</p>
         @endif
+
+        @pushIf($errors->any(), 'notification')
+        <x-partials.toast.default id="rs-add-data-error" message="Gagal memperbaharui data" withTimeout danger />
+        @endPushIf
     @else
         <p class="text-center text-red-500 max-lg:text-sm max-md:text-xs">Belum ada capaian kinerja yang ditugaskan<br>Mohon hubungi admin FTI</p>
     @endif
