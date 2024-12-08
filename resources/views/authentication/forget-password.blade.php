bbc8383c237dd1060a45593fecbd5220c642748e
RozKoy <okkoy.1401@gmail.com>
2024-12-08 11:40:44 +0700

    feat(forget password view): danger toast for forget password error

--- resources/views/authentication/forget-password.blade.php
+++ resources/views/authentication/forget-password.blade.php
@@ -3,4 +3,9 @@
     <x-partials.input.text name="email" title="Email pengguna" autofocus required />
     <x-partials.button.submit title="konfirmasi" />
     <x-partials.link.default route="login" title="masuk" name="Masuk" center />
+
+    @pushIf($errors->any(), 'notification')
+    <x-partials.toast.default id="forget-password-error" message="Proses gagal" withTimeout danger />
+    @endPushIf
+
 </x-auth-template>
