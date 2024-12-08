6b65e58ebe5d87ae572a3b49a4e3084f8bf19665
RozKoy <okkoy.1401@gmail.com>
2024-12-08 08:46:02 +0700

    feat(login view): danger toast for login error

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -5,4 +5,9 @@
     <x-partials.input.password name="password" title="Kata sandi" required />
     <x-partials.link.default route="forget-password" title="lupa kata sandi" name="Lupa Kata Sandi?" right />
     <x-partials.button.submit title="masuk" />
+
+    @pushIf($errors->any(), 'notification')
+    <x-partials.toast.default id="error-login" message="Gagal masuk" withTimeout danger />
+    @endPushIf
+
 </x-auth-template>
