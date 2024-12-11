a6ede3d6bb6c8122d87513fbc86ecfe15301a34f
RozKoy <okkoy.1401@gmail.com>
2024-12-11 17:03:32 +0700

    style: admin user edit view

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -15,16 +15,17 @@
 @endphp
 
 <x-admin-template title="Ubah Pengguna - {{ auth()->user()->unit->name }}">
-    <x-partials.breadcrumbs.default :$breadCrumbs admin />
+    <x-partials.breadcrumbs.default :$breadCrumbs />
     <x-partials.heading.h2 text="ubah pengguna" previous="admin-users" />
+
     <form action="" method="POST" class="flex flex-col gap-2">
         @csrf
         @method('PUT')
-
         <x-partials.label.default for="name" title="Nama pengguna" text="Nama Pengguna" required />
         <x-partials.input.text name="name" title="Nama pengguna" value="{{ $user['name'] }}" autofocus required />
         <x-partials.label.default for="email" title="Email" text="Email" required />
         <x-partials.input.text name="email" title="Email" value="{{ $user['email'] }}" required />
+
         <div id="selection" class="*:rounded-lg *:border *:border-slate-100 *:shadow *:p-1.5 *:gap-1 flex flex-wrap items-center justify-center gap-2 p-2.5 text-primary max-sm:text-sm max-[320px]:text-xs">
             <div class="flex items-center justify-center">
 
@@ -34,7 +35,7 @@
                     <x-partials.input.radio title="Admin semua akses" name="access" id="editor" value="editor" required />
                 @endif
 
-                <label for="editor" title="Admin semua akses">Semua akses</label>
+                <x-partials.label.default for="editor" title="Admin semua akses" text="Semua akses" />
             </div>
             <div class="flex items-center justify-center">
 
@@ -44,7 +45,7 @@
                     <x-partials.input.radio title="Admin akses hanya melihat" name="access" id="viewer-admin" value="viewer" required />
                 @endif
 
-                <label for="viewer-admin" title="Admin akses hanya melihat">Hanya melihat</label>
+                <x-partials.label.default for="viewer-admin" title="Admin akses hanya melihat" text="Hanya melihat" />
             </div>
         </div>
 
@@ -54,4 +55,9 @@
 
         <x-partials.button.edit />
     </form>
+
+    @pushIf($errors->any(), 'notification')
+    <x-partials.toast.default id="user-edit-data-error" message="Gagal memperbarui data pengguna" withTimeout danger />
+    @endPushIf
+
 </x-admin-template>
