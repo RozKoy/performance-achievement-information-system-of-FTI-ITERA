178dcb7c84af3dc0785bc1bf3f7a83b7476cb671
RozKoy <okkoy.1401@gmail.com>
2025-04-27 16:33:51 +0700

    enhance(seeder): delete data template

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UnitSeeder::class,
             UserSeeder::class,
-            // IKUFormat2024Seeder::class,
         ]);
     }
 }
